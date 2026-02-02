24bf87a2f1eeedd8da3bf8c59a58f56b1a113c85
Enzima01 <enzo.h.favaro@gmail.com>
2026-02-02 15:00:15 -0300

    Update implementation

--- src/application/Program.java
+++ src/application/Program.java
@@ -32,10 +32,16 @@ public class Program {
 			System.out.println(obj);
 		}
 		
-		System.out.println("\n=== seller Insert ===");
+		System.out.println("\n=== seller insert ===");
 		Seller newSeller = new Seller(null, "Maria", "maria@gmail.com", new Date(), 3800.00, department);
 		sellerDao.insert(newSeller);
 		System.out.println("Inserted! New id = " + newSeller.getId());
+		
+		System.out.println("\n=== seller update ===");
+		seller = sellerDao.findById(1);
+		seller.setName("Enzo Henrique Favaro");
+		sellerDao.update(seller);
+		System.out.println("Update completed!!");
 		
 		
 
