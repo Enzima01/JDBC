6ed2cbb327a0617d0bb8f4e0dd4395644eba5414
Enzima01 <enzo.h.favaro@gmail.com>
2026-02-02 11:33:37 -0300

    Seller entity class

--- src/application/Program.java
+++ src/application/Program.java
@@ -1,13 +1,18 @@
 package application;
 
+import java.util.Date;
+
 import model.entities.Department;
+import model.entities.Seller;
 
 public class Program {
 
 	public static void main(String[] args) {
 		Department dp = new Department(1, "Books");
 		System.out.println(dp);
-
+		
+		Seller sl = new Seller(1, "Enzo", "enzo@gmail.com", new Date(), 3200.00, dp);
+		System.out.println(sl);
 	}
 
 }
